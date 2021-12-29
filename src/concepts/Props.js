f0908d028312c56a2f8a2316a324dbb8cdc44e6b
GanapriyaS <ganapriyakheersagar@gmail.com>
2021-12-29 23:39:14 +0530

    Props - Children

--- src/concepts/Props.js
+++ src/concepts/Props.js
@@ -12,7 +12,9 @@ export default function Props() {
         <div className='love' >
             <Love title='Pyaar'/>
             <Love title={love.title} number={love.number} img={love.img} />
-            <Love title='Kadhal ' number={141+2} />
+            <Love title='Kadhal ' number={141+2} >
+                <h4>Happy Life</h4>
+            </Love>
         </div>
     )
 }
@@ -30,12 +32,13 @@ const subtitle='Love Urself';
 // };
 
 
-const Love = ({img,title,number}) => {
+const Love = ({img,title,number,children}) => {
     return (
         <article className='love_img'>
             <h2 style={{color:'red'}}>{title}{number}</h2>
             <Image img={img}/>
             <h4>{subtitle.toUpperCase()}</h4>
+            {children}
         </article>
     )
 };
